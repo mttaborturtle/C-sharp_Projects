8a0e3d0223c621da9cf6dcf8863b329c5300090b
Bill Mitcheson <mttaborturtle@gmail.com>
2020-12-22 21:41:09 -0800

    Added assignment

---
+++ shipping_quote.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace shipping_quote.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.\nPlease enter package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            string tooHeavy = weight > 50 ? "Package too heavy to be shipped via Package Express. Have a good day." : "Please enter the package width:";
+            Console.WriteLine(tooHeavy);
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter package length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+            if ((width + height + length) > 50)
+            {
+                Console.WriteLine("Package too big to be shipped by Package Express.");
+            }
+            else
+            {
+                int quote = ((height * width * length) * weight) / 100;
+                Console.WriteLine("Your estimated total for shipping this paackage is: $" + quote.ToString() + ".00\nThank you!");
+            }
+
+        }
+    }
+}
