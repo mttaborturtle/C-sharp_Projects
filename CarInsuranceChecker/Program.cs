ee06c47538d815cebb5ef71019be08037a9a8257
Bill Mitcheson <mttaborturtle@gmail.com>
2020-12-22 18:39:16 -0800

    Added assignment step 168

---
+++ CarInsuranceChecker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CarInsuranceChecker
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int old = Convert.ToInt32(age);
+
+            Console.WriteLine("Have you ever had a DUI? Please type true or false:");
+            string DUI = Console.ReadLine();
+            bool trouble = Convert.ToBoolean(DUI);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string tickets = Console.ReadLine();
+            int law = Convert.ToInt32(tickets);
+
+            Console.WriteLine("Are you qualified?");
+            Console.WriteLine(old > 15 && trouble == false && law <= 3);
+        }
+    }
+}
