a9c0105a9ebd81c66c883e4caf42539acd1291de
Bill Mitcheson <mttaborturtle@gmail.com>
2020-12-22 22:31:19 -0800

    Added assignment DoWhile

---
+++ DoWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Give me a number less than 20 to count up to:");
+            int endNum = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("Number = {0}", i);
+                i++;
+
+                if (i > endNum)
+                    break;
+
+            } while (i < 20);
+        }
+    }
+}
