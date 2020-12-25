1c3e2f4c2a895d75c8e445089ae6eb68268fa781
Bill Mitcheson <mttaborturtle@gmail.com>
2020-12-25 12:41:12 -0800

    Added DoSomeMath assignment

---
+++ DoSomeMath/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoSomeMath
+{
+    public class SomeMaths
+    {
+        public void DoTheMath()
+        {
+            Console.WriteLine("Please enter a number: ");
+            Num1();
+            Console.WriteLine("Please enter a number: ");
+            Num2();
+
+
+            AddMe();
+            subtractMe();
+            MultMe();
+
+
+        }
+
+        public void Num1()
+        {
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void Num2()
+        {
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void AddMe()
+            {
+               
+                //int added = number1 + num2;
+                Console.WriteLine("This is a test");
+            }
+
+        public void subtractMe()
+        {
+            //int subbed = AddMe.num1 - num2;
+            Console.WriteLine("This is a test");
+        }
+
+        public void MultMe()
+        {
+            Console.WriteLine("This is a test");
+        }
+
+
+    }
+}
---
+++ DoSomeMath/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace DoSomeMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeMaths c = new SomeMaths();
+            c.DoTheMath();
+        }
+    }
+
+    
+}
