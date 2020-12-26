08d3b04494ddb44ffa0206460db93ce0a377f2f2
Bill Mitcheson <mttaborturtle@gmail.com>
2020-12-26 13:04:35 -0800

    Updated program.

--- DoSomeMath/Program.cs
+++ DoSomeMath/Program.cs
@@ -7,17 +7,25 @@ namespace DoSomeMath
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please give me a number to do some maths with then hit enter: ");
-            SomeMaths c = new SomeMaths();
-            int input1 = Convert.ToInt32(Console.ReadLine());
-            int Added = c.AddMe(input1);
-            Console.WriteLine("Your number + 5 = " + Added);
-            int Subbed = c.SubtractMe(input1);
-            Console.WriteLine("Your number - 5 = " + Subbed);
-            int Multiplied = c.MultMe(input1);
-            Console.WriteLine("Your number X 5 = " + Multiplied);
-            Console.ReadLine();
-
+            try
+            {
+                Console.WriteLine("Please give me a number to do some maths with then hit enter: ");
+                SomeMaths c = new SomeMaths();
+                int input1 = Convert.ToInt32(Console.ReadLine());
+                int Added = c.AddMe(input1);
+                Console.WriteLine("\nYour number + 5 = " + Added);
+                int Subbed = c.SubtractMe(input1);
+                Console.WriteLine("Your number - 5 = " + Subbed);
+                int Multiplied = c.MultMe(input1);
+                Console.WriteLine("Your number X 5 = " + Multiplied);
+                Console.WriteLine("\nHit enter to continue...");
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("\nThe number entered needs to be a whole number. Please hit enter and try again.");
+                Console.ReadLine();
+            };
         }
     }
 
