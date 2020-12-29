807911a7106b4456e244d97f2b41379cd272c72d
Bill Mitcheson <mttaborturtle@gmail.com>
2020-12-28 17:28:15 -0800

    Added assignment.

---
+++ DoSomeMaths2/Maths.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoSomeMaths2
+{
+    class Maths
+    {
+        public int AddMe(int x)
+        {
+            int result = x + x;
+            return result;
+        }
+
+        public int AddMe(decimal x)
+        {
+            decimal result = x * x;
+            int changed = Convert.ToInt32(result);
+            return changed;
+        }
+
+        public int AddMe(string x)
+        {
+            int y = Convert.ToInt32(x);
+            int result = y - 50;
+            return result;
+        }
+    }
+}
---
+++ DoSomeMaths2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DoSomeMaths2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maths c = new Maths();
+
+            int Added = c.AddMe(250);
+            Console.WriteLine("250 + 250 = {0}", Added);
+
+            int Multed = c.AddMe((decimal)4.5);
+            Console.WriteLine("4.5 X 4.5 = {0}", Multed);
+
+            int Subbed = c.AddMe("200");
+            Console.WriteLine("200 - 50 = {0}", Subbed);
+            Console.ReadLine();
+        }
+    }
+}
