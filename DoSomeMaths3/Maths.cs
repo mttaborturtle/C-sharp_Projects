e3b51ad6e035730117ea63e59515931c9df66eff
Bill Mitcheson <mttaborturtle@gmail.com>
2020-12-28 18:15:47 -0800

    Added assignment

---
+++ DoSomeMaths3/Maths.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoSomeMaths3
+{
+    class Maths
+    {
+        public int AddMe(int x)
+        {
+            int result = x * 10;
+            return result;
+        }
+
+        public int AddMe(int x, int y)
+        {
+            int result = x * y;
+            return result;
+        }
+    }
+}
---
+++ DoSomeMaths3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DoSomeMaths3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maths c = new Maths();
+
+            Console.WriteLine("Enter the first number to multiply: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number to multiply(You may leave this blank): ");
+            string num2 = Console.ReadLine();
+            
+            if (num2 == "")
+            {
+                int Multed = c.AddMe(num1);
+                Console.WriteLine("Number 1 X number 2(10 if blank) = {0}", Multed);
+            }
+            else
+            {
+                int changed = Convert.ToInt32(num2);
+                int Multed = c.AddMe(num1, changed);
+                Console.WriteLine("Number 1 X number 2(10 if blank) = {0}", Multed);
+            }
+            Console.ReadLine();
+        }
+    }
+}
