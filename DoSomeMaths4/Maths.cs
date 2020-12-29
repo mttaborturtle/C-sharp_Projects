5ac8e234c5e2cc3e5af8c8957c3dcaab789297a7
Bill Mitcheson <mttaborturtle@gmail.com>
2020-12-28 20:06:46 -0800

    Added assignment

---
+++ DoSomeMaths4/Maths.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoSomeMaths4
+{
+    class Maths
+    {
+        public void MathMe(int x, int y)
+        {
+            int Mult = x * 50;
+            Console.WriteLine("\nFirst number X 50 = " + Mult);
+            Console.WriteLine("The second number is: " + y);
+        }
+    }
+}
---
+++ DoSomeMaths4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DoSomeMaths4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maths c = new Maths();
+
+            int num1 = 25;
+            int num2 = 30;
+
+            c.MathMe(num1, num2);
+            c.MathMe(x:num1, y:num2);
+            
+            Console.ReadLine();
+        }
+    }
+}
