07109166de2509f94c311979e5991204230bdc72
Bill Mitcheson <mttaborturtle@gmail.com>
2020-12-31 10:24:32 -0800

    Changed assignment

--- StructMe/Program.cs
+++ StructMe/Program.cs
@@ -6,9 +6,13 @@ namespace StructMe
     {
         static void Main(string[] args)
         {
+            //Create a new object from the struct
             Struct struct1 = new Struct();
+            
+            //Give it a value
             struct1.Number = 12.5m;
             
+            //Print it to the console
             Console.WriteLine("This is my decimal number: " + struct1.Number);
             Console.ReadLine();
         }
--- StructMe/Struct.cs
+++ StructMe/Struct.cs
@@ -6,6 +6,7 @@ namespace StructMe
 {
     public struct Struct
     {
+        //Create the struct named Number and assign it the property decimal
         public decimal Number { get; set; }
     }
 }
