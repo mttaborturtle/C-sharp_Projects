ff5320cb53cbe97b94056a224a98a340c7741b04
Bill Mitcheson <mttaborturtle@gmail.com>
2021-01-04 15:52:52 -0800

    Added assignment

---
+++ FileWriter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace FileWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number that you would like to save to the file: number.txt:");
+            string number1 = Console.ReadLine(); //Take in a number and save it to a string variable
+
+            //Write the variable to a file named number.txt
+            File.WriteAllText(@"C:\Users\mttab\Logs\number.txt", number1);
+            
+            //Read the file that you just added the number to
+            string numbFile = File.ReadAllText(@"C:\Users\mttab\Logs\number.txt");
+            
+            //Write the number contained in the file to the console
+            Console.WriteLine("The number read back from the file is: {0}", numbFile);
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOneGame/Program.cs
+++ TwentyOneGame/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOneGame
 {
@@ -10,18 +11,36 @@ namespace TwentyOneGame
     {
         static void Main(string[] args)
         {
+            //string text = "Here is some text I want to write to a file....";
+            //File.WriteAllText(@"C:\Users\mttab\Logs\log.txt", text);
+            //string stuff = File.ReadAllText(@"C:\Users\mttab\Logs\log.txt");
+            //Console.WriteLine(stuff);
+            //string text2 = "This is some stuff to add!!";
+            //File.AppendAllText(@"C:\Users\mttab\Logs\log.txt", text2);
 
 
-            Deck deck = new Deck();
-            deck.Shuffle(3);
-
-            foreach (Card card in deck.Cards)
+            //Main method that starts the game
+            Console.WriteLine("Welcome to the MtTabor Hotel and Casino. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("How much money are you willing to gamble today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to play a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y")
             {
-                Console.WriteLine(card.Face + " of " + card.Suit);
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
             }
-            Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Feel free to go get a drink and come back when you are ready to play. Bye for now.");
             Console.ReadLine();
-
         }
 
         
