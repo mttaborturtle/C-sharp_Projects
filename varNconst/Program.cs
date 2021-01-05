662cc59b2f6b37a10e78312ed4584d1c8253e131
Bill Mitcheson <mttaborturtle@gmail.com>
2021-01-04 22:41:37 -0800

    Updated App

--- varNconst/Program.cs
+++ varNconst/Program.cs
@@ -11,13 +11,13 @@ namespace varNconst
         static void Main(string[] args)
         {
             //Create a new person
-            Person person1 = new Person("Einstein");
+            var person1 = new Person("Einstein");
 
             //Create a constructor you will use later
             const string opener = "The smartest guy ever, who is also one of my quantum physics idols is ";
 
             //Put it all together and print it to the screen
-            Console.WriteLine(opener + person1.first + " " + person1.last);
+            Console.WriteLine(opener + person1.first + " " + person1.last + "!!!!!");
             Console.ReadLine();
         }
 
